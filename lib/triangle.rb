8656d548669ba5833641ecfc8b5eac59cc3ea48e
JeremyKungu <jeremikungu27@gmail.com>
2022-10-24 14:23:57 +0300

    tests passing

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 =length3
+  end
+  def kind
+    validate_triangle
+      if @length1 == @length2 && @length2 == @length3
+        :equilateral
+      elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3 
+        :isosceles
+      else
+        :scalene
+      end      
+  end
+  def greater_than_zero?
+    [@length1, @length2, @length3].all?(&:positive?)
+  end
+
+  def inequality?
+    @length1 + @length2 > @length3 && @length2 + @length3 > @length1 && @length3 + @length1 > @length2
+  end
+
+  def validate_triangle
+    raise TriangleError unless greater_than_zero? && inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
